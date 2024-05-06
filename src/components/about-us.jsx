f56564b75c724de5a39e502c3196d492edec2a17
angel <61515423+angx1@users.noreply.github.com>
2024-05-06 19:07:48 +0200

    Update about-us.jsx

--- src/components/about-us.jsx
+++ src/components/about-us.jsx
@@ -13,6 +13,7 @@ const AboutUs = (props) => {
 
                We are the Polytechnics University ETSISI sports club based in Madrid Spain<br/>, a group of people who want to become<br/> the best version of themselves, 
                a <em><strong>comunity</strong></em> to <em><strong>inspire</strong></em> and to work <em><strong>together</strong></em> to get to wherever we want.<br/><br/>
+               If you wanna get into any of our sport teams or want to now anything more specific send us an email: <strong>clubdeportivo.etsisi@upm.es</strong><br/>
 
                follow us on instagram: <a rel="noreferrer" target="_blank" href="https://www.instagram.com/cdetsisi/?hl=en">@cdetsisi</a><br/>
                our events <a rel="noreferrer" target="_blank" href="https://linktr.ee/cdetsisi">here</a><br/>
