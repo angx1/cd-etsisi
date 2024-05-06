9895f46d01d8f33c50e8c441c7cdf42770daa288
angx1 <angel.rodriguez3rm@gmail.com>
2024-05-06 19:32:49 +0200

    changes to manifesto 2

--- src/components/manifesto.jsx
+++ src/components/manifesto.jsx
@@ -10,10 +10,13 @@ const Manifesto = (props) => {
             <p className="position-absolute top-50 start-50 translate-middle">
                 1. Our mission is progress.<br/> 
                 2. A community to outperform.<br/> 
-                3. We believe on opportunities, hardwork, <br/> 
+                3. Focused on the journey not the destination.<br/>
+                4. We believe on opportunities, hardwork, <br/> 
                 consistency, clarity and mindfulness.<br/> 
-                4. Make it happen. Shock everyone <br/>
-                5. Enjoy the journey not the destination <br/>
+                5. Make it happen. Shock everyone <br/>
+                
+                
+                
             </p>
         </>
      );
